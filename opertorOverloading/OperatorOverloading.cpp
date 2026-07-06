cc3adaed666127e8d08b1581c4217ae9af4884f4
abdelhamewd <abdelhamed>
2026-07-06 19:37:00 +0300

    Added Workflow

--- opertorOverloading/OperatorOverloading.cpp
+++ opertorOverloading/OperatorOverloading.cpp
@@ -1,91 +1,84 @@
-#include <iostream>
 #include <cstring>
-#include <string>
-#include <stdlib.h>
+#include <iostream>
 #include <ostream>
+#include <stdlib.h>
+#include <string>
 
-class complex{
-    private:
-        int real;
-        int img;
-    public:
-    // delegating Constructor
-        complex() : complex(0,0){}
-    // Initialization List 
-        complex(int R , int I) :real{R},img{I}{}
+class complex {
+private:
+  int real;
+  int img;
 
-    // Copy constructor
-        complex(complex &r): real{r.real} , img{r.img} {}
-     // c = c1 + c2
+public:
+  // delegating Constructor
+  complex() : complex(0, 0) {}
+  // Initialization List
+  complex(int R, int I) : real{R}, img{I} {}
 
-     friend complex operator+(complex c1 , complex c2);
-     friend complex operator-(complex c1, complex c2);
-     friend complex operator*(complex c1, complex c2);
-     friend std::ostream &operator<<(std::ostream &o ,complex &c1);
-     friend std::istream& operator>>(std::istream &o ,complex &c1);
+  // Copy constructor
+  complex(complex &r) : real{r.real}, img{r.img} {}
+  // c = c1 + c2
 
-         void add(complex c)
-     {
-        this->img += c.img;
-        this->real += c.real;
-     }
+  friend complex operator+(complex c1, complex c2);
+  friend complex operator-(complex c1, complex c2);
+  friend complex operator*(complex c1, complex c2);
+  friend std::ostream &operator<<(std::ostream &o, complex &c1);
+  friend std::istream &operator>>(std::istream &o, complex &c1);
 
-     void display()
-     {
-        std::cout << "The Real      : " << this->real << std::endl;
-        std::cout << "The Imaginary : " << this->img  << std::endl;
-     }
+  void add(complex c) {
+    this->img += c.img;
+    this->real += c.real;
+  }
+
+  void display() {
+    std::cout << "The Real      : " << this->real << std::endl;
+    std::cout << "The Imaginary : " << this->img << std::endl;
+  }
 };
 
-complex operator+(complex c1, complex c2)
-{
-    complex t;
-    t.real = c1.real + c2.real;
-    t.img = c1.img + c2.img;
-    return t;
+complex operator+(complex c1, complex c2) {
+  complex t;
+  t.real = c1.real + c2.real;
+  t.img = c1.img + c2.img;
+  return t;
 }
 
-complex operator-(complex c1, complex c2)
-{
-    complex t;
-    t.real = c1.real - c2.real;
-    t.img = c1.img - c2.img;
-    return t;
+complex operator-(complex c1, complex c2) {
+  complex t;
+  t.real = c1.real - c2.real;
+  t.img = c1.img - c2.img;
+  return t;
 }
 
-complex operator*(complex c1, complex c2)
-{
-    complex t;
-    t.real = c1.real * c2.real;
-    t.img = c1.img * c2.img;
-    return t;
+complex operator*(complex c1, complex c2) {
+  complex t;
+  t.real = c1.real * c2.real;
+  t.img = c1.img * c2.img;
+  return t;
 }
-std::ostream &operator <<(std::ostream &o, complex &c1)
-{
-    o << c1.real << "+" << c1.img << "I" << std::endl ;
-    return o;
+std::ostream &operator<<(std::ostream &o, complex &c1) {
+  o << c1.real << "+" << c1.img << "I" << std::endl;
+  return o;
 }
-std::istream &operator >>(std::istream &i, complex &c1)
-{
-    i >> c1.real >> c1.img ;
-    return i;
+std::istream &operator>>(std::istream &i, complex &c1) {
+  i >> c1.real >> c1.img;
+  return i;
 }
-int main()
-{
-    complex c1(1000,100) , c2(100,10);
-    // c1.addition(c2);
-   
-    complex c = c1 + c2;
-    complex b = c1 - c2;
-    complex t = c1 * c2;
+int main() {
+  complex c1(1000, 100), c2(100, 10);
+  // c1.addition(c2);
 
-    complex c4;
-    std::cout << "Enter Real:Img" <<std::endl;
-    std::cin >> c4;
-    c1 = c2 + c4;
+  complex c = c1 + c2;
+  complex b = c1 - c2;
+  complex t = c1 * c2;
 
-    std::cout<< c1;
-    std::cout<< c2;
-    std::cout<< c4;
-    return 0;
+  complex c4;
+  std::cout << "Enter Real:Img" << std::endl;
+  std::cin >> c4;
+  c1 = c2 + c4;
+
+  std::cout << c1;
+  std::cout << c2;
+  std::cout << c4;
+  return 0;
 }
--- opertorOverloading/singeleton.cpp
+++ opertorOverloading/singeleton.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 
+class UART {
+private:
+  UART() : data{0} {}
 
+  int data;
 
-class UART{
-    private:
-        UART(): data{0}{}
+public:
+  static UART &getInstance() {
+    static UART Temp_Uart;
+    return Temp_Uart;
+  }
 
-        int data;
-    public:
+  void SendData(int Data) { this->data = Data; }
 
-    static UART& getInstance()
-    {
-        static UART Temp_Uart;
-        return Temp_Uart;
-    }
-
-    void SendData(int Data)
-    {
-        this->data = Data;
-    }
-
-    int GetData()
-    {
-        return this->data;
-    }
+  int GetData() { return this->data; }
 };
 
-int main()
-{
-    UART &u1 =  UART::getInstance();
-    u1.SendData(10);
-    std::cout << u1.GetData() << std::endl;
+int main() {
+  UART &u1 = UART::getInstance();
+  u1.SendData(10);
+  std::cout << u1.GetData() << std::endl;
+}
+#include <iostream>
+
+class UART {
+private:
+  UART() : data{0} {}
+
+  int data;
+
+public:
+  static UART &getInstance() {
+    static UART Temp_Uart;
+    return Temp_Uart;
+  }
+
+  void SendData(int Data) { this->data = Data; }
+
+  int GetData() { return this->data; }
+};
+
+int main() {
+  UART &u1 = UART::getInstance();
+  u1.SendData(10);
+  std::cout << u1.GetData() << std::endl;
 }
